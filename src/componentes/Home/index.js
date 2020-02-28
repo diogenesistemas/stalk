c1b3c09fa1ca8293d9cebf9bfa91418285c14955
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-02-28 17:27:32 -0300

    Adicionando Rotas e Tela de Login

--- src/componentes/Home/index.js
+++ src/componentes/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 import {
     Navbar,
     Input,
@@ -18,7 +19,7 @@ import {
     CardSubtitle,
     Spinner
 } from 'reactstrap';
-import { MdSearch } from 'react-icons/md'
+import { MdSearch, MdStar } from 'react-icons/md'
 
 class Home extends Component {
     state = {
@@ -28,7 +29,7 @@ class Home extends Component {
 
     meteoroDaPaixao = async (evento) => {
         evento.preventDefault();
-        this.setState({carregando:true});
+        this.setState({ carregando: true });
         const form = evento.target;
         const inputGroup = form.children[0];
         const input = inputGroup.children[0];
@@ -47,11 +48,12 @@ class Home extends Component {
 
         const key = "aXzXNGsOTBT393bkgb7d7yyMzNe0pHwjMWNWcxLp";
         const meteoro = await axios(`https://api.nasa.gov/planetary/apod?date=${input.value}&api_key=${key}`);
+        // const meteoro = await axios(`https://api.nasa.gov/planetary/apod?date=${input.value}&api_key=${key}`).catch();
         this.setState({ meteoro: [meteoro.data, ...this.state.meteoro] });
         console.log(meteoro)
         // let regex = /^[0-9]{2}[\/][0-9]{2}[\/][0-9]{4}$/g;
-        this.setState({carregando:false});
-        
+        this.setState({ carregando: false });
+
 
 
 
@@ -60,19 +62,35 @@ class Home extends Component {
         // https://api.github.com/users/diogenesistemas/followers
     }
 
-   
+
     render() {
         return (
             <>
+                <Navbar color="dark">
+                    <Container className="d-flex justify-content-center">
+                        <img
+                            className="rounded-circle border border-white mr-3"
+                            width="50"
+                            src="https://www.thispersondoesnotexist.com/image"
+                            alt="Pessoa Aleatória" />
+                        <span className="text-white">
+                            Logado como
+                                <Link className="text-white font-weight-bold ml-3" to="/">
+                                {this.props.match.params.usuario}
+                            </Link>
+
+                        </span>
+                    </Container>
+                </Navbar>
                 <Navbar color="dark" fixed="bottom">
                     <Container className="d-flex justify-content-center">
                         <Col xs="12" md="6">
                             <Form onSubmit={this.meteoroDaPaixao}>
                                 <InputGroup>
-                                    <Input type="date"/>
+                                    <Input type="date" />
                                     <InputGroupAddon addonType="append">
                                         <Button color="danger">
-                                            {this.state.carregando ?(<Spinner color="light" size="sm"/>) : (<MdSearch size="20px" />)}                                      
+                                            {this.state.carregando ? (<Spinner color="light" size="sm" />) : (<MdSearch size="20px" />)}
                                             {/* <MdSearch size="20px" /> */}
                                         </Button>
                                     </InputGroupAddon>
@@ -82,23 +100,40 @@ class Home extends Component {
                     </Container>
                 </Navbar>
                 {/* Sempre que for retornar algo visual, a arearo funciton retorna com prenteses */}
-                <Container className="mt-3 mb-5">
-                    <Row>
-                        {this.state.meteoro.map((meteoro) => (
-                            <Col className="d-flex" xs="12" md="4">
-                                <Card className="text-white mb-2" color="dark">
-                                    <CardImg top width="100%" height="30%" src={meteoro.url} alt="{meteoro.title}" />
-                                    <CardBody>
-                                        <CardTitle className="h3 text-center">{meteoro.title}</CardTitle>
-                                        <CardSubtitle className="text-muted text-center" >{meteoro.date.split('-').reverse().join('/')}</CardSubtitle>
-                                        <CardText className="text-justify">{meteoro.explanation}</CardText>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                        ))}
-                    </Row>
-                </Container>
-            </>
+                {this.state.carregando ? (
+                    <Container className="h-100 d-flex flex-column justify-content-center align-items-center">
+                        <Spinner color="dark" size="lg" />
+                        <span>Carregando...</span>
+                    </Container>
+                ) : (
+                        <Container className="mt-3 mb-5">
+                            <Row>
+                                {this.state.meteoro.map((meteoro) => (
+                                    <Col className="d-flex" xs="12" md="4">
+                                        <Card className="text-white mb-2" color="dark">
+                                            <CardImg top width="100%" height="30%" src={meteoro.url} alt="{meteoro.title}" />
+                                            <CardBody>
+                                                <CardTitle className="h3 text-center">{meteoro.title}</CardTitle>
+                                                <CardSubtitle className="text-muted text-center" >{meteoro.date.split('-').reverse().join('/')}</CardSubtitle>
+                                                <CardText className="text-justify">{meteoro.explanation}</CardText>
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Container>
+                    )}
+
+
+                {/* Falsy Truth */}
+                {this.state.meteoro.length === 0 && (
+                    <Container className="h-100 d-flex flex-column justify-content-center align-items-center">
+                        <MdStar color="dark" size="150px" />
+                        <h3>Escolha uma data e seja feliz :) </h3>
+                    </Container>
+
+                )}
+            </> // Fragmentt </>
         )
     }
 }
